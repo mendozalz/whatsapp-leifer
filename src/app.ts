dcc9d447b791e7648fd67518541883c2b95672af
Flavio <flavioanez@gmail.com>
2024-08-25 22:18:01 -0500

    firts

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,15 @@ const main = async () => {
     provider.initHttpServer(3002);
 
     provider.http?.server.get('/send-message', handleCtx(async (bot, req, res)=>{
-        // await bot.sendMessage('573022408297', 'Papaito lindo!!!', {});
-        const body = req.body;
+         await bot.sendMessage('573022408297', 'Papaito lindo!!!', {});
+        /* const body = req.body;
         const message = body.message;
         const mediaUrl = body.mediaUrl;
         await bot.sendMessage('573022408297', message, {
             media: mediaUrl
-        })
+        }) */
         
-            res.end("Hola papaito")
+            res.end("Hola Mundo desde Whatsapp API")
     }));
 
     await createBot({
